193980dfbd2f7fed84723114c7f2d4827d55ca05
Chris Mitchell <chris.mitchell@rms.com>
2019-03-30 02:02:28 -0600

    save [skip ci]

--- jest.config.js
+++ jest.config.js
@@ -19,9 +19,4 @@ module.exports = {
     testMatch: [
       '**/test/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)',
     ],
-    // transform: {
-    //   '^.+\\.vue$': 'vue-jest',
-    //   '.+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub',
-    //   '^.+\\.jsx?$': 'babel-jest',
-    // },
   };
