2b6db2f900013d1435c3759da15218d10419c1ae
Christopher Mitchell <cmitchell@crishell.co>
2019-06-27 10:45:13 -0600

    Global Config (#1)

---
+++ test/install.spec.js
@@ -0,0 +1,30 @@
+import { mount } from '@vue/test-utils';
+import Vue from 'vue';
+import VueHubble from '../src';
+
+describe('install.js', () => {
+  beforeEach(() => {
+    process.env.NODE_ENV = 'test';
+  });
+
+  it('should allow the defaultSelectorType to be set', () => {
+    Vue.use(VueHubble, { defaultSelectorType: 'class' });
+
+    const wrapper = mount({
+      template: '<div><span v-hubble="\'selector\'"></span></div>',
+    });
+
+    expect(wrapper.contains('.selector')).toBe(true);
+  });
+
+  it('should handle an invalid defaultSelectorType to be set', () => {
+    Vue.use(VueHubble, { defaultSelectorType: 'invalid' });
+    Vue.prototype.$hubble.defaultSelectorType = 'invalid';
+
+    const wrapper = mount({
+      template: '<div><span v-hubble="\'selector\'"></span></div>',
+    });
+
+    expect(wrapper.contains('[selector]')).toBe(true);
+  });
+});
