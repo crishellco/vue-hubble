b248723ce93444e6ef1f8fae52920cc437c61a77
Christopher Mitchell <cmitchell@crishell.co>
2024-06-27 01:36:30 -0400

    Reapply "Merge pull request #946 from crishellco/cm/vue-3-componsition-api-support"
    This reverts commit 9879b3512c4a9b84f0aea2700ab5283dd879788a.

---
+++ demo/src/components/Buttons.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <button v-hubble="'submit'">Submit</button>
+    <button v-hubble="'cancel'">Cancel</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Buttons',
+
+  setup() {
+    const hubble = 'buttons';
+
+    return {
+      hubble,
+    };
+  },
+};
+</script>
+
+<style scoped>
+button {
+  margin: 10px;
+}
+</style>
